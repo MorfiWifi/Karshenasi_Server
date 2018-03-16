b4c761c92606ac90e7d5fcec1772b44f4143d2f8
MorfiWifi <wifimorfi@gmail.com>
2018-03-16 17:30:13 +0330

    -small Repo chainge

--- karhsenasi_server2/Models/Repo.cs
+++ karhsenasi_server2/Models/Repo.cs
@@ -7,6 +7,7 @@ namespace karhsenasi_server2.Models
 {
     public class Repo
     {
+        private string connect_str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Morfi\\Desktop\\Karshenasi_Server\\karhsenasi_server2\\App_Data\\DB.mdf;Integrated Security=True";
         public void Users() { }
         public void Messages() { }
 
