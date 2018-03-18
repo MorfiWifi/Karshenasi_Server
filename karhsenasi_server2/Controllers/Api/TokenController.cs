4f5b2fac5c689b8a6659f9af151eef10a9b1c33b
Morfi Wifi <wifimorfi@gmail.com>
2018-03-18 21:28:18 +0330

    -small chainge (un necessry)

--- karhsenasi_server2/Controllers/Api/TokenController.cs
+++ karhsenasi_server2/Controllers/Api/TokenController.cs
@@ -54,50 +54,10 @@ namespace karhsenasi_server2.Controllers.Api
             AuthenticationTicket authenticationTicket = new AuthenticationTicket(identity, properties);
             authenticationTicket.Properties.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(1);
 
-            var p = Startup.OAuthOptions;
-            var mm = p.AccessTokenFormat;
+           // var p = Startup.OAuthOptions;
+           // var mm = p.AccessTokenFormat;
             
-            var tok = Startup.OAuthOptions.AccessTokenFormat.Protect(authenticationTicket);
-            string MyToken = Startup.OAuthOptions.AccessTokenFormat.Protect(authenticationTicket);
-            Debug.WriteLine("My OWN CRATED TOKEN : " + MyToken);
-            //incomingCall.Message = MyToken;
-            return Ok(MyToken);
-        }
-
-
-        [HttpPost]
-        [ResponseType(typeof(string))]
-        public IHttpActionResult GetToken(string username , string pass)
-        {
-            var users = Repo.GetInstance().Users();
-            try
-            {
-                var matches = users.Where(x => x.FName == username);
-                Debug.WriteLine("Check Ress : ");
-                if (matches.Count() < 1)
-                {
-                    return BadRequest("Didnt Catch That!");
-                }
-                matches = users.Where(x => x.Pass == pass);
-                if (matches.Count() < 1)
-                {
-                    return BadRequest("Didnt Catch That!");
-                }
-
-                User Accepted_user = matches.First();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("EXCEP : > > " + ex.Message);
-                return BadRequest("Didnt Catch That!");
-            }
-            ClaimsIdentity identity = new ClaimsIdentity("Khers_Bear");
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-            dict.Add("USERNAME", username);
-            dict.Add("PASSWORD", pass);
-            AuthenticationProperties properties = new AuthenticationProperties(dict);
-            AuthenticationTicket authenticationTicket = new AuthenticationTicket(identity, properties);
-            authenticationTicket.Properties.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(1);
+            //var tok = Startup.OAuthOptions.AccessTokenFormat.Protect(authenticationTicket);
             string MyToken = Startup.OAuthOptions.AccessTokenFormat.Protect(authenticationTicket);
             Debug.WriteLine("My OWN CRATED TOKEN : " + MyToken);
             //incomingCall.Message = MyToken;
